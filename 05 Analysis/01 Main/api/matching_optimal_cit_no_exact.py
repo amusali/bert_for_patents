ba7bf3c39704e523415b9c5f9281e32edc626699
Musali <amusali@compasslexecon.com>
2025-10-21 13:08:19 +0200

    adding matching for exact matching for CPC only

--- 05 Analysis/01 Main/api/matching_optimal_cit_no_exact.py
+++ 05 Analysis/01 Main/api/matching_optimal_cit_no_exact.py
@@ -19,6 +19,7 @@ def load_data(citations_path = "/content/drive/MyDrive/PhD Data/08 Citations/03 
               control_path = "/content/drive/MyDrive/PhD Data/10 Sample - pre final/potential_controls_no_exact_match_grantyear_cpc.pkl", 
               clean_ids_path = "/content/drive/MyDrive/PhD Data/10 Sample - pre final/clean_potential_control_ids_no_exact_match_grantyear.csv",
               acq_type_path = "/content/drive/MyDrive/PhD Data/09 Acquired patents/04 All patents.dta",
+              all_patents_metadata = ""
               ):
     
     """Load citations, treated, control data, clean ids, and acquistion type data."""
@@ -35,6 +36,20 @@ def load_data(citations_path = "/content/drive/MyDrive/PhD Data/08 Citations/03 
     if 'control' not in locals():
         control = pd.read_pickle(control_path)
         control['patent_id'] = control['patent_id'].astype(str)
+
+    if 'patents_metadata' not in locals():
+        patents_metadata = pd.read_stata(all_patents_metadata)
+        patents_metadata['patent_id'] = patents_metadata['patent_id'].astype(str)
+    
+    # Merge metadata into control
+    control = control.merge(
+        patents_metadata[['patent_id', 'patent_date']],
+        on='patent_id',
+        how='left'
+    )
+
+    ## Assert all the controls have patent_date column non-missing
+    assert control['patent_date'].isnull().sum() == 0, "Some control patents are missing patent_date"
 
     if 'clean_ids' not in locals():
         clean_ids = pd.read_csv(clean_ids_path)
